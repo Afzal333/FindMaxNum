f9bdde6d83a1a6bd2c241be9cbcb9090f7c7c82a
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-17 08:20:39 +0530

    [Afzal] Added Interger Data to Test

---
+++ FindMaxNumber/FindMaxNumTest/UnitTest1.cs
@@ -0,0 +1,31 @@
+using FindMaxNumber;
+using NUnit.Framework;
+
+namespace FindMaxNumTest
+{
+    public class Tests
+    {
+        FindMaxNum maxNum = new FindMaxNum();
+        [Test]
+        public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFirstMax()
+        {
+            int result = maxNum.FindMaxInteger(15, 5, 10);
+            Assert.AreEqual(15, result);
+        }
+
+        [Test]
+        public void GivenMaxSecondNum_WhenAnalysed_ShouldReturnSecondMax()
+        {
+            int result = maxNum.FindMaxInteger(5, 15, 10);
+            Assert.AreEqual(15, result);
+        }
+
+        [Test]
+        public void GivenMaxThirdNum_WhenAnalysed_ShouldReturnThirdMax()
+        {
+            int result = maxNum.FindMaxInteger(5, 10, 15);
+            Assert.AreEqual(15, result);
+        }
+        
+    }
+}
---
+++ FindMaxNumber/FindMaxNumber/FindMaxNum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FindMaxNumber
+{
+    public class FindMaxNum
+    {
+        public int FindMaxInteger(int a, int b, int c)
+        {
+            if (a > b && a > c)
+                return a;
+            if (b > a && b > c)
+                return b;
+            else
+                return c;
+        }
+       
+    }
+}
